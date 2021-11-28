44343344fba067fb3b289aec0327967a80482ddc
Braulio Bhavamitra <brauliobo@gmail.com>
2021-11-28 14:17:48 -0300

    venom: use post only

--- app/services/whatsapp.rb
+++ app/services/whatsapp.rb
@@ -3,8 +3,12 @@ class Whatsapp
   VENOM_API_URL = "http://localhost:2002"
 
   def self.run code
-    res = HTTPClient.new.get "#{VENOM_API_URL}/eval", input: code
+    res = http.post "#{VENOM_API_URL}/eval", {input: code}, {'Content-type' => 'application/x-www-form-urlencoded'}
     JSON.parse res.body
+  end
+
+  def self.http
+    @http ||= HTTPClient.new
   end
 
 end
--- venom.js
+++ venom.js
@@ -1,24 +1,23 @@
 const env     = require('dotenv').config().parsed
 const venom   = require('venom-bot')
-const express = require('express')
 const fs      = require('fs')
+
+const express    = require('express')
+const bodyParser = require('body-parser')
 
 const SOCK_FILE = 'run/venom.sock'
 
 async function load() {
-  const client = await venom.create(
-    'default',
-    null, null,
-    {
-      refreshQR: 15000,
-      autoClose: 60 * 60 * 24 * 365, //never
-      disableSpins: true
-    }
-  )
+  const client = await venom.create('default', null, null, {
+    refreshQR: 15000,
+    autoClose: 60 * 60 * 24 * 365, //never
+    disableSpins: true
+  })
   const app = express()
+  app.use(bodyParser.urlencoded({ extended: false }))
 
-  app.get('/eval', async (req, res) => {
-    input = req.body?.text ? req.body.text : req.query.input
+  app.post('/eval', async (req, res) => {
+    input = req.body.input
     console.log(`Running ${input}...`)
     try {
       ret = await eval(`(async () => { return ${input}; })()`)
