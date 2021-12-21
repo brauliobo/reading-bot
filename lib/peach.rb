6f6c865d6c74e47cfc22b2387c26391b33aa49e6
Braulio Bhavamitra <brauliobo@gmail.com>
2021-12-20 21:33:43 -0300

    peach: register ractor implementation

--- lib/peach.rb
+++ lib/peach.rb
@@ -1,14 +1,18 @@
 module Enumerable
 
-  def peach threads: nil, priority: nil, wait: true, &block
+  def peach threads: nil, priority: nil, impl: :threads, &block
     block   ||= -> *args {}
     threads ||= (ENV['THREADS'] || '10').to_i
 
     return each(&block) if threads == 1
 
-    pool      = Concurrent::FixedThreadPool.new threads
+    send "#{impl}_each", threads: threads, priority: priority, &block
+  end
+
+  def threads_each threads: nil, priority: nil, &block
+    pool = Concurrent::FixedThreadPool.new threads
     # catch_each can't be used as catchblock needs to be used inside pool.post
-    ret       = each do |*args|
+    ret  = each do |*args|
       pool.post do
         Thread.current.priority = priority if priority
         block.call(*args)
@@ -18,9 +22,31 @@ module Enumerable
     end
 
     pool.shutdown
-    pool.wait_for_termination if wait
-
+    pool.wait_for_termination
     ret
+  end
+
+  ##
+  # Don't work due to Proc#isolate
+  #
+  def ractors_each threads: nil, priority: nil, &block
+    pipe = Ractor.new do
+      loop{ Ractor.yield Ractor.receive }
+    end
+    workers = (1..threads).map do
+      Ractor.new pipe, &block
+    end
+
+    i = 0
+    each do |*args|
+      i += 1
+      pipe << args
+    end
+
+    (1..i).map do
+      _r, ret = Ractor.select(*workers)
+      ret
+    end
   end
 
   def api_peach threads: nil, priority: nil, &block
