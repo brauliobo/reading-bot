1686c4ce27ff293c8c484aea179e1e87b9786eee
Braulio Bhavamitra <brauliobo@gmail.com>
2022-08-07 12:26:56 -0300

    selector: separate numbered check

--- app/services/selector.rb
+++ app/services/selector.rb
@@ -20,12 +20,14 @@ class Selector
     paras.each.with_index do |p, i|
       np = paras[i+1]
 
-      is_head     = p.size < HEADING_LIMIT
-      is_footer   = p =~ /^\d/ && is_head
-      n_is_head   = np && np.size < HEADING_LIMIT
-      n_is_footer = np && np =~ /^\d/ && n_is_head
-      oversize    = i > hi+1 && nt.join.size + p.size > CHARS_LIMIT 
-      middle_head = i > hi+1 && is_head && !is_footer
+      is_head       = p.size < HEADING_LIMIT
+      is_numbered   = p =~ /^\d/
+      is_footer     = is_head && is_numbered
+      n_is_head     = np && np.size < HEADING_LIMIT
+      n_is_numbered = np =~ /^\d/
+      n_is_footer   = n_is_head && n_is_numbered
+      oversize      = i > hi+1 && nt.join.size + p.size > CHARS_LIMIT 
+      middle_head   = i > hi+1 && is_head && !is_footer
 
       break if oversize
       break if middle_head # stop if there is a heading in the middle
